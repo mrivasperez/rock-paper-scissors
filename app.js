1a9210bf91efeb9ce8191ec0ddc5de272c64bbe5
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-04-07 20:13:02 -0700

    added core user choices

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+// User choices
 const ROCK = 'ROCK',
 PAPER = 'PAPER',
-SCISSORS = 'SCISSORS';
+SCISSORS = 'SCISSORS',
+DEFAULT = 'ROCK';
+
+let gameRunning = false;
 
 // Let user choose whether they want r p or s
 
 const getPlayerChoice = function() {
-    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}`, '');
+    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}`, '').toUpperCase();
     if(selection !== ROCK || PAPER || SCISSORS){
-        alert('Invalid choice! I have chosen Rock for you.')
+        alert(`Invalid choice! I have chosen ${DEFAULT} for you.`)
+        return DEFAULT;
     }
+    return selection;
 }
 
 startGameBtn.addEventListener('click', function(){
+    if(gameRunning) {
+        return;
+    }
+    gameRunning = true;
     console.log('Game is starting...')
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
