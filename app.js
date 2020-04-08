39b7fb6ce73669c8a4f40aafec085d5a8c3daa3c
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-04-07 20:02:30 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-start = function() {
-    console.log('Game is starting...')
+const ROCK = 'ROCK',
+PAPER = 'PAPER',
+SCISSORS = 'SCISSORS';
+
+// Let user choose whether they want r p or s
+
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}`, '');
+    if(selection !== ROCK || PAPER || SCISSORS){
+        alert('Invalid choice! I have chosen Rock for you.')
+    }
 }
 
-startGameBtn.addEventListener('click', start)
+startGameBtn.addEventListener('click', function(){
+    console.log('Game is starting...')
+});
